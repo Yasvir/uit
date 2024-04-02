ac2756f24571876d4de7003dfd21cc1d075aa05b
Yasvir <yasvs20@iitk.ac.in>
2024-04-02 12:23:06 +0530

    sfs

---
+++ src/SecondComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import './Second.css'; // Import CSS file
+
+const Second = () => {
+    return (
+        <div className="second">
+            <div className="exp">
+                <div className="head">
+                    <h1>Explore Our Projects!</h1>
+                </div>
+                <div className="text">
+                    Lorem, ipsum dolor amet consectetur adipisicing elit. Voluptates dcz fdvkmdv dkmnvd vsnjdvs jvnds jnvsdkjf fdsgjhbds fdhjds <br/>
+                    laboriosam cupiditate, aperiam ex sint ab minima error vitae placeat, id soluta architecto necessitatibus!
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Second;
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbar from './Navbar';
+import Second from './SecondComponent';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <Navbar/>
+  
+   
+  </React.StrictMode>
+);
+
+
